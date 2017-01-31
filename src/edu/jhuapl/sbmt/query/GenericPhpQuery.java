66a2bf49e8885795c32ae62a7479633ecd77729c
Philip Twu <philip.twu@jhuapl.edu>
2017-01-31 09:26:22 -0500

    Changed query interface to support sum-of-products search type needed for hierarchical search.  The new searchimages.php is backwards compatible with legacy queries with old versions of SBMT.  About to begin testing.

--- src/edu/jhuapl/sbmt/query/GenericPhpQuery.java
+++ src/edu/jhuapl/sbmt/query/GenericPhpQuery.java
@@ -65,8 +65,9 @@ public class GenericPhpQuery extends QueryBase
             String type,
             DateTime startDate,
             DateTime stopDate,
-            List<Boolean> filtersChecked,
-            List<Boolean> camerasChecked,
+            boolean sumOfProductsSearch,
+            List<Integer> camerasSelected,
+            List<Integer> filtersSelected,
             double startDistance,
             double stopDistance,
             double startResolution,
@@ -140,27 +141,11 @@ public class GenericPhpQuery extends QueryBase
             return results;
         }
 
-        List<Integer> filters = new ArrayList<Integer>();
-        for (int i=0; i<filtersChecked.size(); ++i)
+        // No need to do a search if no cameras or filters were selected
+        if(camerasSelected.isEmpty() || filtersSelected.isEmpty())
         {
-            if (filtersChecked.get(i))
-            {
-                filters.add(i+1);
-            }
-        }
-        if (!filtersChecked.isEmpty() && filters.isEmpty())
             return results;
-
-        List<Integer> cameras = new ArrayList<Integer>();
-        for (int i=0; i<camerasChecked.size(); ++i)
-        {
-            if (camerasChecked.get(i))
-            {
-                cameras.add(i+1);
-            }
         }
-        if (!camerasChecked.isEmpty() && cameras.isEmpty())
-            return results;
 
         try
         {
@@ -185,20 +170,36 @@ public class GenericPhpQuery extends QueryBase
             args.put("minPhase", String.valueOf(minPhase));
             args.put("maxPhase", String.valueOf(maxPhase));
             args.put("limbType", String.valueOf(limbType));
-            for (int i=1; i<=filtersChecked.size(); ++i)
+
+            // Populate args for camera and filter search
+            if(sumOfProductsSearch)
             {
-                if (filters.contains(i))
-                    args.put("filterType"+i, "1");
-                else
-                    args.put("filterType"+i, "0");
+                // Sum of products (hierarchical) search: (CAMERA 1 AND FILTER 1) OR ... OR (CAMERA N AND FILTER N)
+                args.put("sumOfProductsSearch", "1");
+                Integer[] camerasSelectedArray = (Integer[])camerasSelected.toArray();
+                Integer[] filtersSelectedArray = (Integer[])filtersSelected.toArray();
+                int numProducts = camerasSelectedArray.length;
+                args.put("numProducts", new Integer(numProducts).toString());
+                for(int i=0; i<numProducts; i++)
+                {
+                    args.put("cameraType"+i, new Integer(camerasSelectedArray[i]+1).toString());
+                    args.put("filterType"+i, new Integer(filtersSelectedArray[i]+1).toString());
+                }
             }
-            for (int i=1; i<=camerasChecked.size(); ++i)
+            else
             {
-                if (cameras.contains(i))
-                    args.put("cameraType"+i, "1");
-                else
-                    args.put("cameraType"+i, "0");
+                // Product of sums (legacy) search: (CAMERA 1 OR ... OR CAMERA N) AND (FILTER 1 OR ... FILTER M)
+                args.put("sumOfProductsSearch", "0");
+                for(Integer c : camerasSelected)
+                {
+                    args.put("cameraType"+(c+1), "1");
+                }
+                for(Integer f : filtersSelected)
+                {
+                    args.put("filterType"+(f+1), "1");
+                }
             }
+
             if (cubeList != null && cubeList.size() > 0)
             {
                 String cubes = "";
@@ -214,7 +215,8 @@ public class GenericPhpQuery extends QueryBase
                 args.put("cubes", cubes);
             }
 
-            results = doQuery("searchimages.php", constructUrlArguments(args));
+            results = doQuery("searchimages-beta.php", constructUrlArguments(args));
+            System.err.println("REMINDER: Currently using searchimages-beta.php");
 
             for (List<String> res : results)
             {
