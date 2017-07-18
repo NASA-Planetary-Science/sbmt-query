736477366f3f056e8423baf6f0664e6ef530cbe8
James Peachey <James.Peachey@jhuapl.edu>
2017-07-18 12:27:30 -0400

    Override getCachedResults so that it correctly finds cached images.

--- src/edu/jhuapl/sbmt/query/eros/NisQuery.java
+++ src/edu/jhuapl/sbmt/query/eros/NisQuery.java
@@ -1,12 +1,18 @@
 package edu.jhuapl.sbmt.query.eros;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import org.joda.time.DateTime;
 
+import edu.jhuapl.saavtk.util.Configuration;
 import edu.jhuapl.sbmt.model.image.ImageSource;
 import edu.jhuapl.sbmt.query.QueryBase;
 
@@ -51,6 +57,7 @@ public class NisQuery extends QueryBase
         return ref;
     }
 
+    @Override
     public QueryBase clone()
     {
         return null;
@@ -69,7 +76,7 @@ public class NisQuery extends QueryBase
     @Override
     public String getImagesPath()
     {
-        return null;
+        return "/NIS/";
     }
 
     /**
@@ -129,7 +136,7 @@ public class NisQuery extends QueryBase
             ImageSource msiSource,
             int limbType)
     {
-        List<String> matchedImages = new ArrayList<String>();
+        List<String> matchedImages = new ArrayList<>();
         List<List<String>> results = null;
 
         double minIncidence = Math.min(fromIncidence, toIncidence);
@@ -144,7 +151,7 @@ public class NisQuery extends QueryBase
             double minScDistance = Math.min(startDistance, stopDistance);
             double maxScDistance = Math.max(startDistance, stopDistance);
 
-            HashMap<String, String> args = new HashMap<String, String>();
+            HashMap<String, String> args = new HashMap<>();
             args.put("startDate", String.valueOf(startDate.getMillis()));
             args.put("stopDate", String.valueOf(stopDate.getMillis()));
             args.put("minScDistance", String.valueOf(minScDistance));
@@ -194,4 +201,33 @@ public class NisQuery extends QueryBase
         return matchedImages;
     }
 
+    @Override
+    protected List<List<String>> getCachedResults(
+            String pathToImageFolder
+            )
+    {
+    	// Create a map of actual files, with key the segment of the
+    	// file name that will match the output of getNisPath.
+        final List<File> fileList = getCachedFiles(pathToImageFolder);
+        final Map<String, File> filesFound = new TreeMap<>();
+        for (File file: fileList)
+        {
+        	// Format for NIS path is /NIS/YYYY/..., basically everything after the
+        	// cache directory prefix.
+            String path = file.getPath().substring(Configuration.getCacheDir().length());
+            filesFound.put(path, file);
+        }
+
+        final List<List<String>> result = new ArrayList<>();
+        SortedMap<String, List<String>> inventory = getImageInventory();
+        // Match the current inventory against the cached file map.
+        for (Entry<String, List<String>> each: inventory.entrySet())
+        {
+            List<String> res = each.getValue();
+            String path = this.getNisPath(res);
+            if (filesFound.containsKey(path))
+                result.add(res);
+        }
+        return result;
+    }
 }
