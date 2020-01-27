d4aff28c412683c861dc4ef2c88d797bbf2f5364
Josh Steele <josh.steele@jhuapl.edu>
2020-01-27 13:26:27 -0500

    Fixed most of the problems mentioned in James' comments in #1828.

--- src/edu/jhuapl/sbmt/query/QueryBase.java
+++ src/edu/jhuapl/sbmt/query/QueryBase.java
@@ -41,6 +41,7 @@ import edu.jhuapl.saavtk.util.DownloadableFileInfo.DownloadableFileState;
 import edu.jhuapl.saavtk.util.FileCache;
 import edu.jhuapl.saavtk.util.FileUtil;
 import edu.jhuapl.saavtk.util.SafeURLPaths;
+import edu.jhuapl.saavtk.util.UrlInfo.UrlStatus;
 
 import crucible.crust.metadata.api.Key;
 import crucible.crust.metadata.api.Metadata;
@@ -222,6 +223,12 @@ public abstract class QueryBase implements Cloneable, MetadataManager, IQueryBas
             String pathToImageFolderOnServer,
             String pathToGalleryFolderOnServer)
     {
+    	DownloadableFileState state = FileCache.refreshStateInfo(pathToFileListOnServer);
+    	if (state.getUrlState().getStatus() != UrlStatus.ACCESSIBLE)
+    	{
+    		return getCachedResults(getDataPath());
+    	}
+
         if (!pathToImageFolderOnServer.endsWith("/"))
             pathToImageFolderOnServer += "/";
 
@@ -229,16 +236,23 @@ public abstract class QueryBase implements Cloneable, MetadataManager, IQueryBas
             pathToGalleryFolderOnServer += "/";
 
         List<List<String>> results = new ArrayList<>();
+
+        if (!checkAuthorizedAccess())
+        {
+            return results;
+        }
+
         File file = FileCache.getFileFromServer(pathToFileListOnServer);
 
-        if (!FileCache.instance().isAccessible(pathToFileListOnServer))
-        {
-            return getCachedResults(getDataPath());
-        }
+//        if (!FileCache.instance().isAccessible(pathToFileListOnServer))
+//        {
+//            return getCachedResults(getDataPath());
+//        }
 
 
         // Let user know that search uses fixed list and ignores search parameters
-        JOptionPane.showMessageDialog(null,
+        if (!Boolean.parseBoolean(System.getProperty("java.awt.headless")))
+        	JOptionPane.showMessageDialog(null,
                 "Search uses a fixed list and ignores all but file name search parameters.",
                 "Notification",
                 JOptionPane.INFORMATION_MESSAGE);
