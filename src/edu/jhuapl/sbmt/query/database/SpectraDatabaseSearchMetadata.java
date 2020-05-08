be3c4c985b13b7f3dd46659e212fd90d339a80f8
Josh Steele <josh.steele@jhuapl.edu>
2020-05-08 13:34:14 -0400

    Took out experimental recursive bounding box code

--- src/edu/jhuapl/sbmt/query/database/SpectraDatabaseSearchMetadata.java
+++ src/edu/jhuapl/sbmt/query/database/SpectraDatabaseSearchMetadata.java
@@ -2,7 +2,6 @@ package edu.jhuapl.sbmt.query.database;
 
 import java.util.List;
 import java.util.TreeSet;
-import java.util.Vector;
 
 import org.joda.time.DateTime;
 
@@ -15,7 +14,6 @@ import crucible.crust.metadata.impl.SettableMetadata;
 public class SpectraDatabaseSearchMetadata extends DatabaseSearchMetadata
 {
     public static final Key<TreeSet<Integer>> CUBE_LIST = Key.of("Cube List");
-    public static final Key<Vector<String>> PATH_LIST = Key.of("Path List");
     public static final Key<String> MODEL_NAME = Key.of("Model Name");
     public static final Key<String> DATA_TYPE = Key.of("Data Type");
 
@@ -28,21 +26,20 @@ public class SpectraDatabaseSearchMetadata extends DatabaseSearchMetadata
     public static SpectraDatabaseSearchMetadata of(String name, DateTime startDate, DateTime stopDate,
             Range<Double> distanceRange, String searchString,
             List<Integer> polygonTypes, Range<Double> incidenceRange,
-            Range<Double> emissionRange, Range<Double> phaseRange, TreeSet<Integer> cubeList, Vector<String> pathList, String modelName, String dataType)
+            Range<Double> emissionRange, Range<Double> phaseRange, TreeSet<Integer> cubeList, String modelName, String dataType)
     {
         FixedMetadata metadata = FixedMetadata.of(createSettableMetadata(name, startDate, stopDate, distanceRange, searchString,
-                                                polygonTypes, incidenceRange, emissionRange, phaseRange, cubeList, pathList, modelName, dataType));
+                                                polygonTypes, incidenceRange, emissionRange, phaseRange, cubeList, modelName, dataType));
         return new SpectraDatabaseSearchMetadata(metadata);
     }
 
     protected static SettableMetadata createSettableMetadata(String name, DateTime startDate, DateTime stopDate,
             Range<Double> distanceRange, String searchString,
             List<Integer> polygonTypes, Range<Double> incidenceRange,
-            Range<Double> emissionRange, Range<Double> phaseRange, TreeSet<Integer> cubeList, Vector<String> pathList, String modelName, String dataType)
+            Range<Double> emissionRange, Range<Double> phaseRange, TreeSet<Integer> cubeList, String modelName, String dataType)
     {
         SettableMetadata metadata = createSettableMetadata(name, startDate, stopDate, distanceRange, searchString, polygonTypes, incidenceRange, emissionRange, phaseRange);
         metadata.put(CUBE_LIST, cubeList);
-        metadata.put(PATH_LIST, pathList);
         metadata.put(MODEL_NAME, modelName);
         metadata.put(DATA_TYPE, dataType);
         return metadata;
